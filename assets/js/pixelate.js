51a94f35913a01ec88cdc328b27aa909d1938e8d
dpstrange <drew@strange.onl>
2020-01-02 09:12:53 -0600

    Update Pixelate.js

--- assets/js/pixelate.js
+++ assets/js/pixelate.js
@@ -1,49 +1,59 @@
 /*
- * pixelate.js - https://github.com/dpstrange/pixelate.js
- * dpstrange
- * JS image pixelation
- * v0.1
- * License: MIT
- */
+ * Name: highlightReel
+ * Dependencies: jQuery
+ * Author: Justin Duke
+ * Author URL: http://jmduke.com
+ * Github URL: https://github.com/jmduke
+ * Licensed under the MIT license
+*/
 
-(function (window, $) {
-    var pixelate = function() {
-        var options = {
-            pixelation: 0.08,
-        };
+(function($) {
+    $.fn.pixelate = function (options) {
+        // TODO:  There's, like, definitely more customization that we could do here.
+        var defaults = {
+            focus: 0.5,
+            canvasID: "pCanvas"
+        }, settings = $.extend({}, defaults, options);
 
-        var element = this;
-        var elementParent = element.parentNode;
+        this.each(function (index) {
+            // Since we pretty much remove the <img> element from the picture pretty quickly, we need to
+            // capture the important attributes before anything else.
+            var width = this.width;
+            var height = this.height;
+  
+            var idSelector = "#" + settings.canvasID + index;
+  
+            // Hide the image itself, since we're transposing over it.
+            $(this).hide();
+  
+            // Edge case: in multiple invocations of pixelate(), be sure to get rid of previous attempts.
+            $(this).parent().children("canvas").remove();
+  
+            $(this).after("<canvas id='" + idSelector.slice(1) + "' class='pixelate'>");
+  
+            $(idSelector).get(0).width = width;
+            $(idSelector).get(0).height = height;
+  
+            var context = $(idSelector).get(0).getContext("2d");
+    
+            // This is how the whole thing works.
+            context.imageSmoothingEnabled = false;
+            // context.mozImageSmoothingEnabled = false;
+            context.webkitImageSmoothingEnabled = false;
+  
+            function pixelate(image) {
+                var el = $("#" + settings.canvasID + index).get(0);
+        
+                var relativeWidth = el.width * settings.focus * 0.25;
+                var relativeHeight = el.height * settings.focus * 0.25;
+        
+                context.drawImage(image, 0, 0, relativeWidth, relativeHeight);
+                context.drawImage(el, 0, 0, relativeWidth, relativeHeight, 0, 0, el.width, el.height);
+            }
 
-        var imgWidth = element.width;
-        var imgHeight = element.height;
+            pixelate(this);
+        });
 
-        var canv = document.createElement('canvas');
-        canv.width = imgWidth;
-        canv.height = imgHeight;
-        canv.classList.add("pixelate");
-
-        var ctx = canv.getContext('2d', { alpha: false });
-        ctx.imageSmoothingEnabled = false;
-
-        var width = imgWidth * options.pixelation;
-        var height = imgHeight * options.pixelation;
-
-        ctx.drawImage(element, 0, 0, width, height);
-        ctx.drawImage(canv, 0, 0, width, height, 0, 0, canv.width, canv.height);
-
-        elementParent.insertBefore(canv, element);
-
-        // element.style.display = 'none';
+        return this;
     };
-
-    document.addEventListener('DOMContentLoaded', function () {
-        var img = document.querySelectorAll('.pixelate');
-
-        for (var i = 0; i < img.length; i++) {
-            img[i].addEventListener('load', function() {
-                pixelate.apply(this);
-            });
-        }
-    });    
-})(window, typeof jQuery === 'undefined' ? null : jQuery);
+}(jQuery));
